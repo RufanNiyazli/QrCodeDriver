b2f1914ad2a4c0377822dc2a417d75acda21518f
RufanNiyazli <developerx73@gmail.com>
2025-07-21 18:28:53 +0400

    start

---
+++ src/main/java/com/project/qrdrive/starter/QrDriveApplication.java
@@ -0,0 +1,13 @@
+package com.project.qrdrive.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrDriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrDriveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/qrdrive/QrDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.qrdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrDriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
